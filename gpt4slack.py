5bac64af662b79636a97c53a9d0427ab905b046d
Kerem Bakir <kerembakir@gmail.com>
2023-03-18 11:58:33 +0100

    add slackbot script

---
+++ gpt4slack.py
@@ -0,0 +1,39 @@
+import os
+import openai
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Initialize the OpenAI API
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize the Slackbot
+app = App()
+
+# Function to interact with ChatGPT
+def chat_gpt(prompt):
+    model_engine = "text-davinci-002"
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    message = completions.choices[0].text.strip()
+    return message
+
+# Event listener for messages
+@app.event("app_mention")
+def handle_app_mentions(body, say):
+    print("Received an app_mention event:", body)
+    text = body['event'].get('text')
+    if text:
+        prompt = f"{text}"
+        response = chat_gpt(prompt)
+        say(response)
+
+# Run the Slackbot
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    handler.start()
